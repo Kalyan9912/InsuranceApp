c227f842eb4324cb6c62ec54b40701a118fc869a
Kalyan9912 <relangimavayya123@gmail.com>
2026-06-02 14:48:14 +0530

    Update Login.jsx

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -1,1 +1,36 @@
+import React, { useState } from 'react';
+import Dashboard from './Dashboard';
 
+function Login() {
+
+  const [login,setLogin] = useState(false);
+
+  const submit = () => {
+      setLogin(true);
+  }
+
+  if(login){
+      return <Dashboard/>
+  }
+
+  return (
+    <div style={{padding:'50px'}}>
+      <h2>Insurance Login</h2>
+
+      <input
+       placeholder="username"
+      /><br/><br/>
+
+      <input
+       type="password"
+       placeholder="password"
+      /><br/><br/>
+
+      <button onClick={submit}>
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
