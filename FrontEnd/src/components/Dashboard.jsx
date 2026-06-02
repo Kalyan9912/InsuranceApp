b106de98a68f485afd69b3511ce1a6edb9789c0f
Kalyan9912 <relangimavayya123@gmail.com>
2026-06-02 14:48:32 +0530

    Update Dashboard.jsx

--- FrontEnd/src/components/Dashboard.jsx
+++ FrontEnd/src/components/Dashboard.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import Policies from './Policies';
 
+function Dashboard(){
+
+ return(
+   <div>
+
+      <h1>Insurance Dashboard</h1>
+
+      <Policies/>
+
+   </div>
+ )
+}
+
+export default Dashboard;
