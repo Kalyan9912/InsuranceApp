6b862b2870dd32d7760fd61d4e0c5c3bd4aeb991
Kalyan9912 <relangimavayya123@gmail.com>
2026-06-02 13:07:15 +0530

    Update index.js

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+import App from './App';
+
+// Global Styles
+import './index.css';
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
