3e2339c51fee46edc8c306dd4cbfcfb9f1539fec
Kalyan9912 <relangimavayya123@gmail.com>
2026-06-02 14:51:27 +0530

    Update PolicyController.java

--- backend/src/main/java/com/insurance/controller/PolicyController.java
+++ backend/src/main/java/com/insurance/controller/PolicyController.java
@@ -1,1 +1,36 @@
+package com.insurance.controller;
 
+import com.insurance.entity.Policy;
+import com.insurance.service.PolicyService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/policies")
+@CrossOrigin("*")
+public class PolicyController {
+
+ private final PolicyService service;
+
+ public PolicyController(
+      PolicyService service){
+
+      this.service=service;
+ }
+
+ @GetMapping
+ public List<Policy> getPolicies(){
+
+    return service.getPolicies();
+ }
+
+ @PostMapping
+ public Policy savePolicy(
+      @RequestBody Policy policy){
+
+    return service.savePolicy(policy);
+ }
+
+}
